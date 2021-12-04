eab9e260be9468a92219a52c3bd2ac3381306458
dtcholak <d.tcholakov@gmail.com>
2021-12-04 16:41:58 -0500

    jump and dash tweaks

--- Assets/Scripts/Movement_Mech.cs
+++ Assets/Scripts/Movement_Mech.cs
@@ -24,7 +24,10 @@ public class Movement_Mech : MonoBehaviour
     public float dashSpeed = 15; //dash speed
     private float speed = 0; //initial velocity
     public float dashdrag = 50; //linear drag after dashing
-    private float abs_speed;
+    private float abs_speed = 0; //absolute value of speed
+    public float fallMultiplier; //gravity multiplier when falling
+    public float lowJumpMultiplier; //gravity multiplier when jumping low;
+    public float flyingMultiplier = 0.9f; // % of original gravity removed when flying
     
 
     [Space]
@@ -32,6 +35,7 @@ public class Movement_Mech : MonoBehaviour
     public bool groundTouch; //boolean player touching ground 
     public bool jumped; //boolean player recently jumped
     public bool isDashing; //boolean player is Dashing
+    public bool flying; //boolean player is flying (dashing while not grounded)
     public int side = 1; //1 is right, -1 left 
 
     [Space]
@@ -41,7 +45,8 @@ public class Movement_Mech : MonoBehaviour
     float initialJumpVelocity_max; //y jump hold velocity variable creation 
     float initialJumpVelocity_min; //y jump tap velocity variable 
     float upGravity_max; //variable creation for upwards gravity (less than downward for "heavy" feel )
-    float downGravity_max; //variable creation for downwards gravity 
+    float downGravity_max; //variable creation for downwards gravity
+    float upGravity_min; //variable creation for upwards gravity when tapping jump 
     float timer = 0; //timer for checking jump timing
     float dragTimer = 0; //timer for putting linear drag back to normal
 
@@ -56,20 +61,22 @@ public class Movement_Mech : MonoBehaviour
         //Jump calculations for gravity
         initialJumpVelocity_max = 2 * MaxJumpHeight / TimetoJumpApex; //calculation for initial jump hold velocity (based on max height and time to apex)
         
-        upGravity_max = -2 * MaxJumpHeight / Mathf.Pow(TimetoJumpApex, 2.0f); //upward gravity calculated to apex of jump based on 0 velocity at apex and set time to apex  
+        upGravity_max = -2 * MaxJumpHeight / Mathf.Pow(TimetoJumpApex, 2.0f); //upward gravity for hold jump button calculated to apex of jump based on 0 velocity at apex and set time to apex  
+        Physics2D.gravity = Vector2.up * upGravity_max; //set gravity for max jump as default
         downGravity_max = -2 * MaxJumpHeight / Mathf.Pow(TimetoJumpDrop, 2.0f); //downward gravity calculated to apex of jump based on 0 velocity at apex and set time to fall
-
-        initialJumpVelocity_min = Mathf.Sqrt(2 * Mathf.Abs(upGravity_max * MinJumpHeight)); //calculation for initial jump tap velocity (based on upward gravity calc and set jump tap height)
-
-        //Debug.Log(initialJumpVelocity_max);
-        //Debug.Log(initialJumpVelocity_min);
-        //Debug.Log(upGravity_max);
-        //Debug.Log(downGravity_max);
+        fallMultiplier = downGravity_max / upGravity_max; //gravity multiplier to fall faster
+        //initialJumpVelocity_min = Mathf.Sqrt(2 * Mathf.Abs(upGravity_max * MinJumpHeight)); //calculation for initial jump tap velocity (based on upward gravity calc and set jump tap height)
+        upGravity_min = Mathf.Pow(initialJumpVelocity_max, 2.0f) / (-2 * MinJumpHeight); //upward gravity for jump button tap
+        lowJumpMultiplier = upGravity_min / upGravity_max; //gravity multiplier for tap jump
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(rb.velocity);
+        //Debug.Log(upGravity_max);
+        //Debug.Log(downGravity_max);
+        //Debug.Log(upGravity_min);
 
         Vector2 dir = control.dir; //vector setting of raw x + y 
         float x = dir.x; //raw x
@@ -84,20 +91,35 @@ public class Movement_Mech : MonoBehaviour
         if (Input.GetButtonDown("Fire3") && !isDashing)   //if left shift button is pressed
         {
             Dash(dash_x,dash_y);    //dash at dashspeed and add linear drag after a delay
+            
         }
         if (isDashing)
         {
             dragTimer += Time.deltaTime;
+
         }
-        if (dragTimer > 0.7)
+        if (dragTimer > 0.5)
         {
             rb.drag = 0;
             isDashing = false;
             dragTimer = 0;
+            if (coll.onGround)
+            {
+                flying = false;
+            }
+            else{
+                flying = true;
+            }
+
+        }
+        if (flying)
+        {
+            //Physics2D.gravity = Vector2.up * -0.5f;
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (flyingMultiplier - 1) * Time.deltaTime;
         }
         if (!isDashing)
         {   
-        
+        flying = false;
         Walk(dir);
         
         if (Input.GetButtonDown("Jump") && coll.onGround && !jumped) //if jump pressed, and touching ground, and haven't recently jumped
@@ -105,6 +127,7 @@ public class Movement_Mech : MonoBehaviour
             Jump(initialJumpVelocity_max); //jump at the speed of initial jump velocity max
             anim.SetTrigger("jump"); //set jump trigger to active in animation script
         }
+        /*
         if (Input.GetButtonUp("Jump")) //if jump released 
         {
             if (rb.velocity.y > initialJumpVelocity_min) //if current rigid body y velocity is greater than initial jump tap velocity 
@@ -113,22 +136,31 @@ public class Movement_Mech : MonoBehaviour
             jumped = false; //reset so no jump has recently happened
             }
         }
-        }
-            Vector2 Velocity = rb.velocity; //new variable for current velocity
+        */
 
-        if(rb.velocity.y > 0) //if current rb y velocity is greater than 0 
+        
+            Vector2 Velocity = rb.velocity; //new variable for current velocity
+        
+
+        if (!flying)
         {
-            Physics2D.gravity = new Vector2(0, upGravity_max); //set physics gravity to upward gravity max
+            //Physics2D.gravity = Vector2.up * upGravity_max;
+        if(rb.velocity.y < -0.01) //if current rb y velocity is greater than 0 
+        {
+            //Physics2D.gravity = new Vector2(0, upGravity_max); //set physics gravity to upward gravity max
             //Velocity.y += upGravity_max * Time.deltaTime;
-            //rb.velocity = Velocity;
-        }else
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            //Debug.Log(fallMultiplier);
+        }else if(rb.velocity.y > 0.01 && !Input.GetButton("Jump"))
         {
-            Physics2D.gravity = new Vector2(0, downGravity_max); //set physics gravity to downward gravity max
+            //Physics2D.gravity = new Vector2(0, downGravity_max); //set physics gravity to downward gravity max
             //Velocity.y += downGravity_max * Time.deltaTime;
-            //rb.velocity = Velocity;
+            //Debug.Log(lowJumpMultiplier);
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
         }
         
-        
+        }
 
         if (coll.onGround && !groundTouch) //if touching ground and weren't before (landing)
         {
@@ -147,7 +179,7 @@ public class Movement_Mech : MonoBehaviour
             timer += Time.deltaTime; //increment timer
             
         } */
-        if(timer > 1.4) //if more than 1.4 seconds have passed
+        if(timer > 0.5) //if more than 1.4 seconds have passed
         {
             jumped = false; //impossible to have recently jumped
             timer = 0; //set timer to zero
@@ -182,7 +214,7 @@ public class Movement_Mech : MonoBehaviour
         speed = rb.velocity.x;
         
         abs_speed = Mathf.Abs(speed);
-        Debug.Log(abs_speed);
+        //Debug.Log(abs_speed);
         if(coll.onLeftWall && dir.x <= 0) //if touching left wall and left input 
         {
             rb.velocity = new Vector2(0, rb.velocity.y); //set x velocity to 0 
@@ -220,10 +252,10 @@ public class Movement_Mech : MonoBehaviour
 
     private void Jump(float Jump_initialVelocity) //creation of jump initial velocity 
     {
-
-        rb.velocity = new Vector2(rb.velocity.x, Jump_initialVelocity); //set rigid body velocity to current rb x velocity and input to function
+        rb.velocity = Vector2.right * rb.velocity;
+        rb.velocity += Vector2.up * Jump_initialVelocity; //set rigid body velocity to current rb x velocity and y velocity input to function
         jumped = true; //recently jumped
-
+        //Debug.Log(Physics2D.gravity);
         jumpParticle.Play(); //jump particle animation
     }
 
@@ -237,18 +269,18 @@ public class Movement_Mech : MonoBehaviour
         //rb.velocity = Vector2.zero; //set rigid body velocity to a zero vector
         Vector2 dash_dir = new Vector2(x, y); //get the input directions and assign them as current
         rb.velocity += dash_dir.normalized * dashSpeed; //make the velocities of magnitude one and multiply by the set dash speed 
-        StartCoroutine(DashDrag());
+        StartCoroutine(DashDrag(dash_dir));
 
      
 
     }
 
-    IEnumerator DashDrag()
+    IEnumerator DashDrag(Vector2 dash_dir)
     {
     yield return new WaitForSeconds(0.2f); //wait half a second for dash to occur
     //Vector2 dir = new Vector2(x, y); //get input directions and assign them as current
-    //rb.velocity -= dir.normalized * dashdecay; //make the velocities of magnitude one and subtract by set dash decay to slow 
-    rb.drag = dashdrag;
+    rb.velocity -= dash_dir.normalized * dashSpeed * (0.75f); //make the velocities of magnitude one and subtract by set dash decay to slow 
+    //rb.drag = dashdrag;
     
     }
 }
